62b49723a57892134677ab00d8c1adf22f6f063b
NafisaNitu <49776498+NafisaNitu@users.noreply.github.com>
2025-05-24 23:24:33 +0600

    Update Offer.jsx

--- src/components/Offer.jsx
+++ src/components/Offer.jsx
@@ -13,7 +13,7 @@ function Offer() {
         <div className="container mx-auto 2xl:px-32 sm:px-16 px-6">
             <div className='md:flex justify-center items-center xl:gap-8'>
 
-                <a href="" className="md:w-1/4 w-full xl:h-24 lg:h-28 sm:h-32 h-24 hover:box-shadow relative after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-full after:h-[4px] hover:after:bg-[#1B9FCB] hover:scale-[1.01] transition duration-300 rounded-sm py-2 flex flex-col items-center gap-3">
+                <a href="" className="md:w-1/4 w-full xl:h-24 lg:h-28 sm:h-32 h-24 hover:box-shadow dark:bg-white dark:text-white relative after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-full after:h-[4px] hover:after:bg-[#1B9FCB] hover:scale-[1.01] transition duration-300 rounded-sm py-2 flex flex-col items-center gap-3">
                    <i className="fa-regular fa-file-lines text-[#1B9FCB] text-4xl h-10"></i>
                     <h3 className="font-medium 2xl:text-lg xl:text-md text-sm text-center tracking-wider">Eligibility Assessment</h3>
                 </a>
